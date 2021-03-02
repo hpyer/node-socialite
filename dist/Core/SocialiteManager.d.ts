3cf3d3b7126545fcb6dd90f07b5bcb70e10431ea
Hpyer <me@hpyer.cn>
2021-03-02 17:13:53 +0800

    build

--- dist/Core/SocialiteManager.d.ts
+++ dist/Core/SocialiteManager.d.ts
@@ -1,6 +1,9 @@
 import { SupportProviders, ProviderConstructable, ProviderConfig, SocialiteConfig, ResolvedProviders, CustomProviderCreators, ProviderCreator } from "../Types/global";
 import Config from "./Config";
 import ProviderInterface from "./ProviderInterface";
+import Github from "../Providers/Github";
+import WeChat from "../Providers/WeChat";
+import WeWork from "../Providers/WeWork";
 export default class SocialiteManager {
     protected config: Config;
     protected resolved: ResolvedProviders;
@@ -8,7 +11,9 @@ export default class SocialiteManager {
     protected providers: SupportProviders;
     constructor(config: SocialiteConfig);
     setConfig(config: SocialiteConfig): void;
-    create(name: string): ProviderInterface;
+    create(name: 'github'): Github;
+    create(name: 'wechat'): WeChat;
+    create(name: 'wework'): WeWork;
     createProvider(name: string): ProviderInterface;
     buildProvider(provider: ProviderConstructable, config: ProviderConfig): ProviderInterface;
     callCustomCreator(provider: string, config: ProviderConfig): ProviderInterface;
