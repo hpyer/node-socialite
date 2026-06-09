6797e5959562b050192cd06d2e236f9ff9709b3c
Hpyer <me@hpyer.cn>
2026-06-09 13:39:56 +0800

    fix: 修复企业微信授权失败的问题

--- src/Providers/WeWork.ts
+++ src/Providers/WeWork.ts
@@ -3,7 +3,7 @@
 import { ProviderConfig } from "../../types/global";
 import { BaseProvider } from "../Core/BaseProvider";
 import { User } from "../Core/User";
-import { buildQueryString } from "../Core/Utils";
+import { buildQueryString, merge } from "../Core/Utils";
 
 /**
  * @see [网页授权登录](https://developer.work.weixin.qq.com/document/path/91335)
@@ -45,6 +45,9 @@ export class WeWork extends BaseProvider
     return this.setAgentId(agentId);
   }
 
+  /**
+   * 开启获取用户敏感信息（仅在 scope 为 snsapi_privateinfo 时有效）
+   */
   detailed(): this
   {
     this._detailed = true;
@@ -68,7 +71,14 @@ export class WeWork extends BaseProvider
     let token = await this.getApiAccessToken();
     let user = await this.getUser(token, code);
     if (this._detailed) {
-      user = await this.getUserById(user['UserId']);
+      let userTicket = user['user_ticket'] ?? '';
+      if (user['userid']) {
+        user = await this.getUserById(token, user['userid']);
+      }
+      if (userTicket) {
+        // 补充敏感信息，头像、性别、手机、邮箱、企业邮箱、员工个人二维码、地址
+        user = merge(user, await this.getUserDetail(token, userTicket));
+      }
     }
     return this.mapUserToObject(user)
       .setProvider(WeWork.NAME)
@@ -93,9 +103,12 @@ export class WeWork extends BaseProvider
         corpsecret: this._config.get('corp_secret') || this._config.get('corpsecret') || this.getClientSecret(),
       }
     });
+    if (!response || !response.data) {
+        throw new Error(`Failed to createApiAccessToken with error response: ${JSON.stringify(response)}`);
+    }
     let data = response.data;
-    if (!data || data['errcode']) {
-      throw new Error(`Failed to get api access_token: ${data['errmsg'] || 'Unknown'}`)
+    if (data['errcode']) {
+      throw new Error(`Failed to createApiAccessToken: ${data['errmsg'] || 'Unknown'}`)
     }
     return data['access_token'];
   }
@@ -103,43 +116,72 @@ export class WeWork extends BaseProvider
   protected async getUser(token: string, code: string): Promise<Record<string, any>>
   {
     let response = await this.doRequest({
-      url: this._baseUrl + '/cgi-bin/user/getuserinfo',
+      url: this._baseUrl + '/cgi-bin/auth/getuserinfo',
       method: 'get',
       params: {
         access_token: token,
         code,
       }
     });
-    let data = response.data;
-    if (!data || data['errcode'] || (!data['UserId'] && !data['OpenId'])) {
-      throw new Error(`Failed to get user openid: ${data['errmsg'] || 'Unknown'}`)
+    if (!response || !response.data) {
+        throw new Error(`Failed to getUser with error response: ${response.data || response}`);
     }
-    else if (!data['UserId']) {
+    let data = response.data;
+    if (data['errcode'] || (!data['userid'] && !data['openid'])) {
+      throw new Error(`Failed to getUser: ${data['errmsg'] || 'Unknown'}`)
+    }
+    else if (!data['user_ticket']) {
       this._detailed = false;
     }
     return data;
   }
 
-  protected async getUserById(userId: string): Promise<Record<string, any>>
+  protected async getUserById(token: string, userId: string): Promise<Record<string, any>>
   {
     let response = await this.doRequest({
       url: this._baseUrl + '/cgi-bin/user/get',
       method: 'get',
       params: {
-        access_token: this.getApiAccessToken(),
+        access_token: token,
         userid: userId,
       }
     });
-    let data = response.data;
-    if (!data || data['errcode'] || !data['userid']) {
-      throw new Error(`Failed to get user: ${data['errmsg'] || 'Unknown'}`)
+    if (!response || !response.data) {
+        throw new Error(`Failed to getUserById with error response: ${response.data || response}`);
     }
+    let data = response.data;
+    if (data['errcode'] || !data['userid']) {
+      throw new Error(`Failed to getUserById: ${data['errmsg'] || 'Unknown'}`)
+    }
+    return data;
+  }
+
+  protected async getUserDetail(token: string, userTicket: string): Promise<Record<string, any>>
+  {
+    let response = await this.doRequest({
+      url: this._baseUrl + '/cgi-bin/auth/getuserdetail',
+      method: 'post',
+      params: {
+        access_token: token,
+      },
+      data: {
+        user_ticket: userTicket,
+      },
+    });
+    if (!response || !response.data) {
+        throw new Error(`Failed to getUserDetail with error response: ${response}`);
+    }
+    let data = response.data;
+    if (data['errcode'] || !data['userid']) {
+      throw new Error(`Failed to get getUserDetail: ${data['errmsg'] || 'Unknown'}`)
+    }
+
     return data;
   }
 
   getAuthUrl(): string
   {
-    let query = {
+    let query: Record<string, any> = {
       appid: this.getClientId(),
       redirect_uri: this._redirectUrl,
       response_type: 'code',
@@ -156,7 +198,9 @@ export class WeWork extends BaseProvider
     }
     if (this._asQrcode) {
       delete query.scope;
-      return `https://open.work.weixin.qq.com/wwopen/sso/qrConnect?` + buildQueryString(query);
+      delete query.response_type;
+      query.login_type = 'CorpApp';
+      return `https://login.work.weixin.qq.com/wwlogin/sso/login?` + buildQueryString(query);
     }
     return 'https://open.weixin.qq.com/connect/oauth2/authorize?' + buildQueryString(query) + '#wechat_redirect';
   }
@@ -183,7 +227,7 @@ export class WeWork extends BaseProvider
     }
 
     return new User({
-      id: user['UserId'] || user['OpenId'] || null,
+      id: user['userid'] || user['openid'] || null,
     });
   }
 
